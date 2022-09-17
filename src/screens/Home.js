2f393bbb249aea74a51db1621437a18855b132a6
kerlorsenglao <kerlorsenglao@gmail.com>
2022-09-17 14:20:03 +0700

    finished install axios and animatable

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,11 +2,14 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 
+import * as Animatable from 'react-native-animatable'
+
 export default function Home() {
     const navigation = useNavigation()
   return (
     <View>
       <Text>Home</Text>
+      <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Up and down you go</Animatable.Text>
       <TouchableOpacity
         style={{
             padding: 5,
