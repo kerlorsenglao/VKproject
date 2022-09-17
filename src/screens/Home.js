d5d8550ea1b8928e274f0e1b6ce2c4a04a93bb4a
kerlorsenglao <kerlorsenglao@gmail.com>
2022-09-17 14:50:20 +0700

    finished install spiner

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,24 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import * as Animatable from 'react-native-animatable'
 import Config from 'react-native-config'
 
 import IonIcon from 'react-native-vector-icons/Ionicons'
+import Spinner from 'react-native-loading-spinner-overlay'
 
 export default function Home() {
     const navigation = useNavigation()
+    const [spiner, setSpiner] = useState(true)
+    useEffect(()=>{
+      setTimeout(() => {
+        setSpiner(false)
+      }, 5000);
+    })
   return (
     <View>
+      <Spinner visible={spiner}/>
       <Text>Home = {Config.TEST}</Text>
       <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Up and down you go</Animatable.Text>
       <IonIcon name='alarm-outline' size={50} color="black"/>
