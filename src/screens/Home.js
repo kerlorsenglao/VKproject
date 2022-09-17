27559b91e45237d139200513d24145393a3465a3
kerlorsenglao <kerlorsenglao@gmail.com>
2022-09-17 14:46:13 +0700

    finished install vector-icon

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,12 +5,15 @@ import { useNavigation } from '@react-navigation/native'
 import * as Animatable from 'react-native-animatable'
 import Config from 'react-native-config'
 
+import IonIcon from 'react-native-vector-icons/Ionicons'
+
 export default function Home() {
     const navigation = useNavigation()
   return (
     <View>
       <Text>Home = {Config.TEST}</Text>
       <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Up and down you go</Animatable.Text>
+      <IonIcon name='alarm-outline' size={50} color="black"/>
       <TouchableOpacity
         style={{
             padding: 5,
