6bee15ee1c5bd175159141d4e7658816fddecb07
kerlorsenglao <kerlorsenglao@gmail.com>
2022-09-17 14:32:30 +0700

    finished install Config and call from .env file

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,12 +3,13 @@ import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import * as Animatable from 'react-native-animatable'
+import Config from 'react-native-config'
 
 export default function Home() {
     const navigation = useNavigation()
   return (
     <View>
-      <Text>Home</Text>
+      <Text>Home = {Config.TEST}</Text>
       <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Up and down you go</Animatable.Text>
       <TouchableOpacity
         style={{
